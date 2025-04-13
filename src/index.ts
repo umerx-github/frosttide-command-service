0d65530a0f29d4c89867cb549139131a3e5f64c6
Andrew Dawes <andrewjamesdawes@gmail.com>
2025-04-13 19:24:16 +0000

    Forward message to Kafka topic

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,29 @@
 import { WebSocketServer, WebSocket } from 'ws';
+import { Producer, ReconnectingProducerAdapter } from '@umerx/kafkajs-client';
+
+const producer = new ReconnectingProducerAdapter({
+    producer: new Producer({
+        kafkaConfig: {
+            brokers: ['broker:9092'],
+            clientId: 'frosttide-command-service',
+            connectionTimeout: 30,
+            requestTimeout: 30,
+            enforceRequestTimeout: true,
+            retry: {
+                retries: 0,
+            },
+            // logLevel: logLevel.DEBUG,
+        },
+        producerConfig: {
+            allowAutoTopicCreation: false,
+            idempotent: true,
+            retry: {
+                retries: Number.MAX_SAFE_INTEGER,
+            },
+        },
+        topics: ['frosttide-command-service-output'],
+    }),
+});
 
 const wss = new WebSocketServer({ port: 8080 });
 
@@ -7,6 +32,7 @@ wss.on('connection', (ws: WebSocket) => {
 
     ws.on('message', (message: string) => {
         console.log(`Received message: ${message}`);
+        producer.sendMessage({ key: 'myKey', value: message });
         wss.clients.forEach((client) => {
             client.send(`Server received your message: ${message}`);
         });
